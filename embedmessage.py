173a407983d9f6c35996a05b2337f0c44d6f82ca
Mali <pmalihd@gmail.com>
2023-12-19 10:51:31 +0100

    Create embedmessage.py

---
+++ embedmessage.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+#Config
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+#you can get your token from https://discord.com/developers/applications/ and create a new application and bot
+bot_token ="YOUR TOKEN HERE"
+
+#Event that triggers when the bot is online
+@client.event
+async def on_ready():
+    print(f'Successful Logged in as {client.user.name}')
+    print("------------------------------------")
+
+#Command that triggers when you write "!hello"
+@client.event
+async def on_message(message):
+    if message.content.startswith('!mcserver'):
+
+        embedVar = discord.Embed(title="**This is a Embed Message**", description="", color=0xD88E61)
+        embedVar.add_field(name="**Hello**", value="Hello World", inline=False)
+        embedVar.add_field(name="", value="Hello again!", inline=False)
+
+        await message.delete() #deletes the message that triggered the command
+        await message.channel.send(embed=embedVar)
+
+
+#Run the bot with your token
+client.run(bot_token)
